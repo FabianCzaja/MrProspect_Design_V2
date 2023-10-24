2252d2b1b4d9e978b06c4cd3559141ff768dea32
Fabian Czaja <fabian.czaja@new-solutions.com>
2023-10-24 14:13:52 +0200

    continued Landing Page

--- js/main.js
+++ js/main.js
@@ -44,103 +44,29 @@ document.onscroll = function (e) {
         '%'
     );
 
-    document.getElementById("circularcursor").style.setProperty(
-        'top', (
-            e.clientY + window.scrollY
-        ) +
-    'px'
-    );
+    if(ElementInViewport(document.getElementById("how-it-works"))) {
+      document.getElementById("spotlight-left").style.display = "block";
+      document.getElementById("spotlight-right").style.display = "block";
+    }else {
+      document.getElementById("spotlight-left").style.display = "none";
+      document.getElementById("spotlight-right").style.display = "none";
+
+    }
 }
 
-var pointCount = 10 // количесвто точек(кривизна линии)
-var lineCount = 30 // количество линий
+ function ElementInViewport (el) {
 
-var random = d3.randomNormal(30, 25); // разброс точек
-
-
-var svg = d3.select('svg');    
-var NumberLines = d3.range(0, lineCount)
- var w = 500,
-     h = 100;
-
-var x = d3.scaleLinear()
-      .domain([0, pointCount - 1])
-      .range([0, w]);
-
-var y = d3.scaleLinear()
-      .domain([0, 50])
-      .range([0, h]);
-
-var prevData = d3.range(pointCount).map(random); // Путь для первой линии
-var data = d3.range(lineCount).map((i)=>{
-
-  if (i == 0) {
-    return prevData;
-  } else {
-    var next_data = prevData.map((v, k,e) => {
-  // var now = e[k];
-  // var next = e[k + 1];
-  //     console.log(now) // Задаток для проверки соседних точек 
-      
-      return v + Math.random() * 2;
-   
-    });
-  
-    prevData = next_data;
-    return next_data;
+  // Special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
   }
-}); // Путь для последующих 
 
-var vector = d3.range(lineCount).map((i)=>{
-  return d3.range(pointCount).map((j)=>{
-    return data[i][j] < 10 ? 1 : -1;
-  });
-}); // Определяет куда идет линия ( верх или вниз) и меняет направление
+  var rect = el.getBoundingClientRect();
 
-
-var line = d3.line()
-      .x(function(d, i) {
-        return x(i);
-      })
-      .y(function(d, i) {
-        return y(d);
-      });
-
-  var numerPath = svg.append('g')
-      .selectAll("path")
-      .data(NumberLines)
-      .enter()
-      .append('path')
-
-var path = svg.selectAll('path')
-      .datum(data[0])
-      .attr("d", line.curve(d3.curveBasis));
-
-
-function update () {
-  var transition = d3.transition()
-    .duration(500)
-    .ease(d3.easeSin);
-  var z = 1
-
-  path.each(function(d, i){
-
-    data[i] = data[i].map((v, k) => {
-      if (vector[i][k] > 0 && v > 50) {
-        vector[i][k] = -1
-      }
-      if (vector[i][k] < 0 && v < 10) {
-        vector[i][k] = 1
-      } 
-      return v + z * vector[i][k];
-    });
-
-      d3.select(this)
-        .datum(data[i])
-        .transition(transition)
-        .attr("d", line.curve(d3.curveBasis));
-    });
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+  );
 }
- d3.interval(() => {
-   update();
- }, 100);
