a15973109b5b795470f47718dbd7da45b47d7341
Fabian <fabian.czaja@quality-devs.com>
2023-10-25 07:14:08 +0200

    continued

--- js/main.js
+++ js/main.js
@@ -10,63 +10,62 @@ window.onload = (event) => {
     }
 };
 
-document.body.onmousemove = function (e) {
+document.body.onmousemove = function(e) {
     document.getElementById("circularcursor").style.setProperty(
         'left', (
             e.clientX + window.scrollX
         ) +
-    'px'
+        'px'
     );
     document.getElementById("circularcursor").style.setProperty(
         'top', (
             e.clientY + window.scrollY
         ) +
-    'px'
+        'px'
     );
 }
 
-document.onscroll = function (e) {
+document.onscroll = function(e) {
     document.getElementById("horizontal-text-scroll-primary").style.setProperty(
         'left',
-        window.innerWidth/window.scrollY*400 +
+        window.innerWidth / window.scrollY * 400 +
         '%'
     );
 
     document.getElementById("horizontal-text-scroll-secondary").style.setProperty(
         'right',
-        window.innerWidth/window.scrollY*400 +
+        window.innerWidth / window.scrollY * 400 +
         '%'
     );
 
     document.getElementById("horizontal-text-scroll-third").style.setProperty(
         'left',
-        window.innerWidth/window.scrollY*400 +
+        window.innerWidth / window.scrollY * 400 +
         '%'
     );
 
-    if(ElementInViewport(document.getElementById("how-it-works"))) {
-      document.getElementById("spotlight-left").style.display = "block";
-      document.getElementById("spotlight-right").style.display = "block";
-    }else {
-      document.getElementById("spotlight-left").style.display = "none";
-      document.getElementById("spotlight-right").style.display = "none";
-
+    if (ElementInViewport(document.getElementById("how-it-works"))) {
+        document.getElementById("spotlight-left").style.display = "block";
+        document.getElementById("spotlight-right").style.display = "block";
+    } else {
+        document.getElementById("spotlight-left").style.display = "none";
+        document.getElementById("spotlight-right").style.display = "none";
     }
 }
 
- function ElementInViewport (el) {
+function ElementInViewport(el) {
 
-  // Special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-      el = el[0];
-  }
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
 
-  var rect = el.getBoundingClientRect();
+    var rect = el.getBoundingClientRect();
 
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-  );
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
 }
